f0f96f1aa07fa50ee8eed348612d8ad5c60d0f62
Francesco Pace <pace.francesco.g@gmail.com>
2015-06-25 22:37:13 +0200

    disabled version assert

--- setup.py
+++ setup.py
@@ -27,8 +27,8 @@ def get_version():
             if line.startswith('__version__'):
                 ret = eval(line.strip().split(' = ')[1])
                 assert ret.count('.') == 2, ret
-                for num in ret.split('.'):
-                    assert num.isalnum(), ret
+                # for num in ret.split('.'):
+                #     assert num.isdigit(), ret
                 return ret
         else:
             raise ValueError("couldn't find version string")
